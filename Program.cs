c3362c8cdaf6318a94b54feeff4f7ab8e0da7a76
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-05 16:28:41 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 7. Solicite al usuario el ingreso de su nombre y presente el mensaje "Hola [nombre]"
+            Console.WriteLine("Por favor, ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine($"Hola {nombre.ToUpper()}");
+            Console.WriteLine("Ingrese una tecla para salir");          
+            Console.ReadKey();
+        }
+    }
+}
